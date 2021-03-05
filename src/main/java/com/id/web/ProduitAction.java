7e9eb2da5428b7d531615befac8af3d1ab0b3cb0
LELO <nyaka.lelo@econocom.com>
2021-03-05 03:11:40 +0100

    Ajout crud actions

--- src/main/java/com/id/web/ProduitAction.java
+++ src/main/java/com/id/web/ProduitAction.java
@@ -1,12 +1,102 @@
 package com.id.web;
 
+import java.util.List;
+
+import org.modelmapper.ModelMapper;
+
+import com.id.dto.ProduitDto;
+import com.id.entites.Produit;
+import com.id.service.ICatalogueService;
+import com.id.service.SingletonService;
 import com.opensymphony.xwork2.ActionSupport;
 
 //Dans Struts le ModelAction(les actions) hérite de la classe ActionSupport
 public class ProduitAction extends ActionSupport {
+	private ProduitDto produitDto = new ProduitDto();
+	private Produit produit = new Produit();
+	private List<Produit> produits;
+	private String ref;
+	private boolean editMode = false;
+	private ICatalogueService service = SingletonService.getService();
+
+	/*
+	 * ModelMapper modelMapper = new ModelMapper(); produit =
+	 * modelMapper.map(produitDto, Produit.class);
+	 */
 	public String index( ) {
 		//SUCCESS est une constante héritée de ACtionSupport
+		produits = service.listProduit();
 		return SUCCESS;
 	}
+	
+	public String save() {
+		if(editMode == false) 
+		service.ajouterProduit(produit);
+		else
+		service.miseAjourProduit(produit);
+		produits = service.listProduit();
+		return SUCCESS;
+	}
+	
+	public String delete() {
+		service.supprimerProduit(ref);
+		produits = service.listProduit();
+		return SUCCESS;
+	}
+	
+	public String edit() {
+		editMode = true;
+		produit = service.recupererProduit(ref);
+		service.miseAjourProduit(produit);
+		produits = service.listProduit();
+		return SUCCESS;
+	}
+	
+	public Produit getProduit() {
+		return produit;
+	}
+	
+	
+	public String getRef() {
+		return ref;
+	}
+
+	public void setRef(String ref) {
+		this.ref = ref;
+	}
+
+	public void setProduit(Produit produit) {
+		this.produit = produit;
+	}
+
+
+
+	public List<Produit> getProduits() {
+		return produits;
+	}
+
+
+	public void setProduits(List<Produit> produits) {
+		this.produits = produits;
+	}
+
+
+	public ProduitDto getProduitDto() {
+		return produitDto;
+	}
+	public void setProduitDto(ProduitDto produitDto) {
+		this.produitDto = produitDto;
+	}
+
+	public boolean isEditMode() {
+		return editMode;
+	}
+
+	public void setEditMode(boolean editMode) {
+		this.editMode = editMode;
+	}
+	
+	
+	
 
 }
--- src/main/webapp/views/Produits.jsp
+++ src/main/webapp/views/Produits.jsp
@@ -4,15 +4,52 @@
 <head>
 <meta charset="UTF-8">
 <title>Produits</title>
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
 </head>
 <body>
- <s:form action="save" method="post">
- 	<s:textfield label="REF" name="reference"></s:textfield>
- 	<s:textfield label="Designation" name="designation"></s:textfield>
- 	<s:textfield label="Prix" name="prix"></s:textfield>
- 	<s:textfield label="Quantite" name="quantite"></s:textfield>
- 	<s:checkbox label="Promo" name="promo"></s:checkbox>
- 	<s:submit value="Save"></s:submit>
- </s:form>
+<div>
+	 <s:form action="save" method="post">
+	 	<s:textfield label="REF" name="produit.reference"></s:textfield>
+	 	<s:textfield label="Designation" name="produit.designation"></s:textfield>
+	 	<s:textfield label="Prix" name="produit.prix"></s:textfield>
+	 	<s:textfield label="Quantite" name="produit.quantite"></s:textfield>
+	 	<s:checkbox label="Promo" name="produit.promo"></s:checkbox>
+	 <!--  	<s:textfield name="editMode"></s:textfield> permet de voir une valeur du model  -->
+	 	<s:hidden name="editMode"></s:hidden>
+ 		<s:submit value="Save"></s:submit>
+	 </s:form>
+</div>
+<div>
+	<table class="table1">
+	 <tr>
+	 	<th>REF</th>
+	 	<th>DES</th>
+	 	<th>PRIX</th>
+	 	<th>QUANTITE</th>
+	 	<th>PROMO</th>
+	 </tr>
+	 <s:iterator value="produits">
+		 <tr>
+		 	<td><s:property value="reference"/></td>
+		 	<td><s:property value="designation"/></td>
+		 	<td><s:property value="prix"/></td>
+		 	<td><s:property value="quantite"/></td>
+		 	<td><s:property value="promo"/></td>
+		 	<s:url namespace="/" action="delete" var="lien1">
+		 		<s:param name="ref">
+		 			<s:property value="reference"/>
+		 		</s:param>
+		 	</s:url>
+		 	<s:url namespace="/" action="edit" var="lien2">
+		 		<s:param name="ref">
+		 			<s:property value="reference"/>
+		 		</s:param>
+		 	</s:url>
+		 	<td><s:a href="%{lien1}">Suppr</s:a></td>
+		 	<td><s:a href="%{lien2}">Edit</s:a></td>
+		 </tr>
+	 </s:iterator>
+	</table>
+</div>
 </body>
 </html>
